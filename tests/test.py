605da4540a46fa55be39326586587a3449688fee
Daniel Lwetabe <dannylwe11@gmail.com>
2021-08-08 21:21:38 +0300

    creating mocks

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 import requests
+import responses
 
 # I’ll be using the Zippopotam.us REST API. 
 # This API takes a country code and a zip code and 
@@ -27,3 +28,24 @@ def test_get_locations_for_us_90210_check_one_place_is_returned():
      response_body = response.json()
      assert len(response_body["places"]) == 1
 
+# creating mocks
+@responses.activate
+def test_simulate_data_cannot_be_found():
+    responses.add(
+        responses.GET,
+        "http://api.zippopotam.us/us/9999999999",
+        json={"error": "No data exists for US zip code 9999999999"},
+        status=404
+    )
+    response = requests.get("http://api.zippopotam.us/us/9999999999")
+    assert response.status_code == 404
+    response_body = response.json()
+    assert response_body["error"] == "No data exists for US zip code 9999999999"
+
+# If, during testing, you accidentally hit an endpoint that 
+# does not have an associated mock response, 
+# you’ll get a ConnectionError
+@responses.activate
+def test_unmatched_endpoint_raises_connectionerror():
+    with pytest.raises(ConnectionError):
+        requests.get('http://api.zippopotam.us/us/12345')
