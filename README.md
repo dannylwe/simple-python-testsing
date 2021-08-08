c0d8306d51f2d1fa9e56523d83f54ff71af72580
Daniel Lwetabe <dannylwe11@gmail.com>
2021-08-08 20:58:19 +0300

    setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # simple-python-testsing
+## Install and run application
+```
+$ pip install pipenv
+$ pipenv shell
+$ pipenv install
+```
+## run tests
+```
+$ pytest ./tests/test.py
+```
---
+++ tests/test.py
@@ -0,0 +1,8 @@
+import requests
+
+# I’ll be using the Zippopotam.us REST API. 
+# This API takes a country code and a zip code and 
+# returns location data associated with that country and zip code.
+
+def test_get_locations_for_us_90210_check_status_code_equals_200():
+    pass
